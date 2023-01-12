3b7af91535059b678411edf727f6d844f6c71c95
Shivanshp4s <79980567+Shivanshp4s@users.noreply.github.com>
2023-01-12 19:19:27 +0530

    Update Slingshot.js

--- Slingshot.js
+++ Slingshot.js
@@ -13,7 +13,11 @@ class SlingShot{
         this.SlingShot= Constraint.create(options);
         World.add(world, this.SlingShot);
     }
+   fly(){
+      
+     this.SlingShot.bodyA = null; 
 
+    }
       display(){
 
         if ( this.SlingShot.bodyA ){
@@ -27,9 +31,5 @@ class SlingShot{
         }   
         
     }
-    fly(){
-      
-     this.SlingShot.bodyA = null; 
-
-    }
+    
 }
