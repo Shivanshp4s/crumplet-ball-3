dae46e4bf0c4c39d680335a0d1f546b9cd7f2f3e
Shivanshp4s <79980567+Shivanshp4s@users.noreply.github.com>
2023-01-12 19:20:56 +0530

    Update Paper.js

--- Paper.js
+++ Paper.js
@@ -1,38 +1,21 @@
-class Paper
-{
-	constructor(x,y,r)
-	{
-		var options={
-			isStatic:false,
-			restitution:0.3,
-			friction:0,
-			density:1.2
-
-			
-			}
-		this.x=x;
-		this.y=y;
-		this.r=r
-		this.image=loadImage("paper.png");
-		this.body=Bodies.circle(this.x, this.y, (this.r-20)/2, options)
-		World.add(world, this.body);
-
-	}
-	display()
-	{
-			
-			var paperpos=this.body.position;		
-
-			push()
-			translate(paperpos.x, paperpos.y);
-			rectMode(CENTER)
-			
-			fill(255,0,255)
-			imageMode(CENTER);
-			image(this.image, 0,0,this.r, this.r)
-			
-			pop()
-			
-	}
-
+class Paper{
+    constructor() {
+        var options = {
+            'restitution':0,
+            'friction':0,
+            'density':1.0
+        }
+        this.body = Bodies.circle(250,540,20,options);
+        this.width = 33;
+        this.image = loadImage("paper.png");
+        World.add(world, this.body);
+      }
+      display(){
+        var pos = this.body.position;
+        var angle = this.body.angle;
+        fill("white");
+        rotate(angle);
+        imageMode(CENTER);
+        image(this.image,pos.x,pos.y,33,33);
+    }
 }
